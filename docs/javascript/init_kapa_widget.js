32c9f5f45d41aa410a49eb0d5633b37c1251cec9
he3als <65787561+he3als@users.noreply.github.com>
2024-06-05 19:08:53 +0100

    fix: actual kapa fix 😱

--- docs/javascript/init_kapa_widget.js
+++ docs/javascript/init_kapa_widget.js
@@ -6,5 +6,18 @@ document.addEventListener("DOMContentLoaded", function () {
     script.setAttribute("data-project-color", "#2894F3");
     script.setAttribute("data-project-logo", "https://avatars.githubusercontent.com/u/78708182?s=128");
     script.async = true;
+
+    script.onload = () => {
+        const kapaLoadedCheck = setInterval(() => {
+            const kapaStyle = document.head.querySelector('style[data-emotion="mantine"]');
+            if (kapaStyle && kapaStyle.sheet) {
+                clearInterval(kapaLoadedCheck);
+                const cssRules = Array.from(kapaStyle.sheet.cssRules).map(rule => rule.cssText).join('\n');
+                const node = document.body.appendChild(kapaStyle);
+                node.appendChild(document.createTextNode(cssRules));
+            }
+        }, 150);
+    };
+
     document.head.appendChild(script);
 });
